e3911a952d5f32bf258b958efc71df8d19b12043
ChaconEfrain <efrachaga@gmail.com>
2023-02-07 12:39:59 -0600

    Change postgre url

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
+  `postgresql://postgres:d5ZDox8rMAFP2m0tSHj4@containers-us-west-25.railway.app:6989/railway`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
